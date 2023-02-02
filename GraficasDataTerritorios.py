c8d9f3cb6946fffe495a5df89477cfa7256ab0e3
Geovanny <cali.arborea@gmail.com>
2023-02-02 14:54:26 -0500

    Definición de datos de nacimientos en las distintas zonas del norte de Cali

--- GraficasDataTerritorios.py
+++ GraficasDataTerritorios.py
@@ -4,7 +4,13 @@
 # In[ ]:
 
 
-# importar libreira matplotlib
+# importar libreria matplotlib
 
 import matplotlib.pyplot as plt
+
+# Definición de datos de nacimientos en las distintas zonas por año
+
+year = [2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
+birthsLaFlora = [154,163,156,165,127,141,142,194,197,197,215,184,177]
+birthsSantaMonica = [42,25,39,27,28,33,34,37,43,33,27,36,37]
 
