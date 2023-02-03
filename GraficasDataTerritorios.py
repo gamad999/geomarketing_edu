3bad40191eff94a19dbf3f0f6bbf5616eda60af0
Geovanny <cali.arborea@gmail.com>
2023-02-02 23:36:35 -0500

    Ajuste graficas nacimientos norte, oeste y sur de Cali

--- GraficasDataTerritorios.py
+++ GraficasDataTerritorios.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[16]:
 
 
 # importar libreria matplotlib
@@ -16,6 +16,7 @@ birthsSantaMonica = [42,25,39,27,28,33,34,37,43,33,27,36,37]
 birthsOeste = [119,128,110,110,112,138,131,128,129,160,175,196,189]
 birthsElLido = [312,327,346,330,332,318,341,377,368,398,417,418,392]
 birthsElIngenio = [230,210,243,210,245,217,262,263,264,274,293,301,296]
+birthsCiudadJardin = [230,210,243,210,245,217,262,263,264,274,293,301,296]
 
 
 # Configuración de caracteristicas del grafico
@@ -24,10 +25,11 @@ plt.plot(year, birthsSantaMonica, label = 'Zona Santa Monica', linewidth = 3, co
 plt.plot(year, birthsOeste, label = 'Zona Oeste', linewidth = 3, color = 'green')
 plt.plot(year, birthsElLido, label = 'Zona El Lido', linewidth = 3, color = 'purple')
 plt.plot(year, birthsElIngenio, label = 'Zona El Ingenio', linewidth = 3, color = 'black')
+#plt.plot(year, birthsCiudadJardin, label = 'Zona Ciudad Jardín', linewidth = 3, color = 'yellow')
 
 
 # Definición de título y nombres de ejes
-plt.title('Numero de nacimientos zonas norte entre 2005-2017')
+plt.title('Numero de nacimientos zonas norte, oeste y sur entre 2005-2017')
 plt.ylabel('Nacimientos')
 plt.xlabel('Años')
 
