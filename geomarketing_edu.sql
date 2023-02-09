26e98b2d07fd66b0e30cbd52cf5f9a27ec3f72ef
Geovanny <cali.arborea@gmail.com>
2023-02-08 23:25:16 -0500

    Calculo de caracteristicas para implementacion de modelo de ML bosques aleatorios regresion parte 1

--- geomarketing_edu.sql
+++ geomarketing_edu.sql
@@ -220,9 +220,14 @@ CREATE INDEX sidx_manz_censal_jf_geom
   USING gist
   (geom);
 
+/* Construcción tabla de número de manzanas por barrio*/
+
 SELECT cod_barr AS cod_barrio, barrio, zona, COUNT(DISTINCT id) AS manzanas FROM manz_censal_jf GROUP BY cod_barrio, barrio, zona ORDER BY manzanas DESC;
 
-SELECT cod_barr, barrio, zona from manz_censal_jf group by cod_barr, barrio, zona order by barrio;
+--SELECT cod_barr, barrio, zona from manz_censal_jf group by cod_barr, barrio, zona order by barrio;
+
+-- Construcción de tabla número de manzanas clasificadas por estratos 5 y 6 por barrio
+
 SELECT (t1.cod_barr, t2.cod_barr), (t1.barrio, t2.barrio), t1.manz_est5, t2.manz_est6
 
 FROM
@@ -233,6 +238,62 @@ FULL JOIN
 (SELECT cod_barr, barrio, COUNT(DISTINCT id) AS manz_est6 FROM manz_censal_jf WHERE estrato = 6 GROUP BY cod_barr, barrio ORDER BY manz_est6
 ) AS t2 ON t1.barrio = t2.barrio ORDER BY t1.manz_est5 DESC;
 
+-- Construcción de tabla número de manzanas clasificadas por estratos 5 y 6 por zonas para la confección de gráficas estadísticas
+
+-- SELECT zona, COUNT(DISTINCT id) AS tot_manzanas FROM manz_censal_jf WHERE estrato = 6 GROUP BY zona ORDER BY tot_manzanas DESC;
+
+SELECT t1.zona, t1.tot_manz, t1.barrios, t2.manz_est5, t3.manz_est6
+
+FROM
+(SELECT zona, COUNT(DISTINCT id) AS tot_manz, COUNT(DISTINCT barrio) AS barrios FROM manz_censal_jf GROUP BY zona ORDER BY tot_manz, barrios
+) AS t1
+
+FULL JOIN
+(SELECT zona, COUNT(DISTINCT id) AS manz_est5 FROM manz_censal_jf WHERE estrato = 5 GROUP BY zona ORDER BY manz_est5
+) AS t2 ON t1.zona = t2.zona
+
+FULL JOIN
+(SELECT zona, COUNT(DISTINCT id) AS manz_est6 FROM manz_censal_jf WHERE estrato = 6 GROUP BY zona ORDER BY manz_est6
+) AS t3 ON t2.zona = t3.zona;
+
+
+-- Cálculo de población femenina en edad reproductiva residentes en viviendas de estratos 5 o 6 por barrio y zona para el año 2018 de realización del Censo del DANE 
+
+-- Creación de tabla que alojará las estadisticas para la implementación del modelo de machine learning de aprendizaje supevisado con el algoritmo de bosques aleatorios regresion multiple
+
+CREATE TABLE estadisticas_ml(id serial primary key, cod_barr varchar(4), barrio varchar(70), zona varchar(30), mjedrp18 int);
+
+INSERT INTO estadisticas_ml(
+SELECT ROW_NUMBER() OVER (ORDER BY 1), cod_barr, barrio, zona, (SUM(mujr15_19) + SUM(mujr20_24) + SUM(mujr25_29) + SUM(mujr30_34) + SUM(mujr35_39) + SUM(mujr40_44)) AS MjEdRp18 
+FROM manz_censal_jf GROUP BY cod_barr, barrio, zona ORDER BY MjEdRp18 DESC);
+
+-- Calculo de áreas de la capa de barrios en hectareas
+
+ALTER TABLE barrios ADD COLUMN area_has double precision;
+
+UPDATE barrios SET area_has = (SELECT ST_Area(barrios.geom)/10000);
+
+-- Migración de datos de numero de viviendas por estrato 5 y 6 desde tabla inicial a la tabla de atributos de la capa de barrios
+
+ALTER TABLE barrios
+ADD COLUMN viv18_est5 int, ADD COLUMN viv18_est6 int, ADD COLUMN viv20_est5 int, ADD COLUMN viv20_est6 int, ADD COLUMN viv22_est5 int, ADD COLUMN viv22_est6 int;
+
+UPDATE barrios SET viv18_est5 = viviendas_year.viv18_est5 FROM viviendas_year WHERE barrios.id_barrio = viviendas_year.cod_barrio;
+UPDATE barrios SET viv18_est6 = viviendas_year.viv18_est6 FROM viviendas_year WHERE barrios.id_barrio = viviendas_year.cod_barrio;
+UPDATE barrios SET viv20_est5 = viviendas_year.viv20_est5 FROM viviendas_year WHERE barrios.id_barrio = viviendas_year.cod_barrio;
+UPDATE barrios SET viv20_est6 = viviendas_year.viv20_est6 FROM viviendas_year WHERE barrios.id_barrio = viviendas_year.cod_barrio;
+UPDATE barrios SET viv22_est5 = viviendas_year.viv22_est5 FROM viviendas_year WHERE barrios.id_barrio = viviendas_year.cod_barrio;
+UPDATE barrios SET viv22_est6 = viviendas_year.viv22_est6 FROM viviendas_year WHERE barrios.id_barrio = viviendas_year.cod_barrio;
+
+ALTER TABLE barrios
+ADD COLUMN viv18_tot int, ADD COLUMN viv20_tot int, ADD COLUMN viv22_tot int;
+
+UPDATE barrios SET viv18_tot = viv18_est5 + viv18_est6;
+UPDATE barrios SET viv20_tot = viv20_est5 + viv20_est6;
+UPDATE barrios SET viv22_tot = viv22_est5 + viv22_est6;
+
+
+-- Creación de campos de numero de viviendas por estrato 
 
 
 
